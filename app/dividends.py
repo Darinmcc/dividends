fb6cb69be24d915221ac5819a94d7284c98429d3
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-07-01 01:19:43 -0400

    write to csv

--- app/dividends.py
+++ app/dividends.py
@@ -32,16 +32,6 @@ ref_response = requests.get(symbol_ref_url) #< response variable - sends get req
     #print(ref_response.text) #same as print(response.json())
 ref_parsed_response = json.loads(ref_response.text)
 
-
-dividend_url = f"https://cloud.iexapis.com/stable/stock/{symbol}/dividends/ytd?token={api_key}"
-dividend_response = requests.get(dividend_url) #< response variable - sends get requests, specify the URL for the request - see documentation#
-    #print(type(dividend_response)) #<class 'requests.models.Response'>
-    #print(dividend_response.status_code) #200
-    #print(type(dividend_response.text))
-    #print(dividend_response.text) #same as print(response.json())
-dividend_parsed_response = json.loads(dividend_response.text) # variable, parse str to dict
-#print(dividend_parsed_response)
-
 ref_dictionary = []
 
 for symbols in ref_parsed_response:
@@ -52,49 +42,49 @@ if symbol.upper() not in ref_dictionary:
     print("Not a valid stock symbol. Please re-run")
     exit()
 else:
+    dividend_url = f"https://cloud.iexapis.com/stable/stock/{symbol}/dividends/next?token={api_key}"
+    dividend_response = requests.get(dividend_url) #< response variable - sends get requests, specify the URL for the request - see documentation#
+        #print(type(dividend_response)) #<class 'requests.models.Response'>
+        #print(dividend_response.status_code) #200
+        #print(type(dividend_response.text))
+        #print(dividend_response.text) #same as print(response.json())
+    dividend_parsed_response = json.loads(dividend_response.text) # variable, parse str to dict
+        #print(dividend_parsed_response)
     print(dividend_parsed_response)
 
+#exDate	string	refers to the dividend ex-date
+#paymentDate	string	refers to the payment date
+#recordDate	string	refers to the dividend record date
+#declaredDate	string	refers to the dividend declaration date
+#amount	number	refers to the payment amount
+    #flag	string	Type of dividend event
+#currency	string	Currency of the dividend
+#description	string	Description of the dividend event
+#frequency	string	Frequency of the dividend
 
-#USE TO CYCLE THROUGH LIST FOR SYMBOLS
-#print(price_parsed_response[0]["symbol"])
+print(dividend_parsed_response["exDate"])
 
-#breakpoint()
-
-#--csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "dividends.csv")
  #don't change csv file path or __file__ variable
  #file starts in app directory
 
-#--csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+csv_headers = ["Symbol","Ex Date", "Payment Date", "Record Date", "Declared Date", "Dividend Amount", "Dividend Event Type","Currency","Description","Frequency"]
 
-#--with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    #--writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    #--writer.writeheader() # uses fieldnames set above
-    #--for date in dates:
-        #--daily_prices = tsd[date]
-        #--writer.writerow({
-            #-"timestamp": date, 
-            #-"open": daily_prices["1. open"], 
-            #-"high": daily_prices["2. high"], 
-            #-"low": daily_prices["3. low"], 
-            #-"close": daily_prices["4. close"], 
-            #-"volume": daily_prices["5. volume"]})
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({
+        "Symbol": dividend_parsed_response["symbol"], 
+        "Ex Date": dividend_parsed_response["exDate"], 
+        "Payment Date": dividend_parsed_response["paymentDate"], 
+        "Record Date": dividend_parsed_response["recordDate"], 
+        "Declared Date": dividend_parsed_response["declaredDate"], 
+        "Dividend Amount": dividend_parsed_response["amount"],
+        "Dividend Event Type": dividend_parsed_response["flag"],
+        "Currency": dividend_parsed_response["currency"],
+        "Description": dividend_parsed_response["description"],
+        "Frequency": dividend_parsed_response["frequency"]})
 
-
-#print("-------------------------")
-#print("SELECTED SYMBOL: XYZ")
-#print("-------------------------")
-#print("REQUESTING STOCK MARKET DATA...")
-#print("REQUEST AT: 2018-02-20 02:00pm")
-#print("-------------------------")
-#print("LATEST DAY: 2018-02-20")
-#print("LATEST CLOSE: $100,000.00")
-#print("RECENT HIGH: $101,000.00")
-#print("RECENT LOW: $99,000.00")
-#print("-------------------------")
-#print("RECOMMENDATION: BUY!")
-#print("RECOMMENDATION REASON: TODO")
-#print("-------------------------")
-#print("HAPPY INVESTING!")
 print("-------------------------")
 
 
